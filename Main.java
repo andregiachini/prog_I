83eea06df390d700bb453befb8971f134333790b
andregiachini <56172743+andregiachini@users.noreply.github.com>
2020-03-09 11:42:07 -0300

    add Main.java first progs

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String args[]){
+
+    int i = 4;
+    int j = 5;
+        System.out.println("i + j = "+(i+j));
+        System.out.println();
+        for(i=0;i<51;i++){
+            System.out.println("Java " + i);
+
+        }
+        System.out.println();
+        for(i=1;i<11;i++){
+            System.out.println(i + "x" + j + "= " + (i*j));
+        }
+    }
+}
