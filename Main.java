e6721ffd354ba20680533cb72db9a27bd4f62012
andregiachini <56172743+andregiachini@users.noreply.github.com>
2020-03-10 15:43:28 -0300

    add questao 8 java L0

--- Main.java
+++ Main.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
+
 public class Main{
     public static void main(String args[]){
 
-    int i = 4;
-    int j = 5;
-        System.out.println("i + j = "+(i+j));
-        System.out.println();
-        for(i=0;i<51;i++){
-            System.out.println("Java " + i);
+        Scanner ler = new Scanner(System.in);
 
-        }
-        System.out.println();
-        for(i=1;i<11;i++){
-            System.out.println(i + "x" + j + "= " + (i*j));
+        //int nasc = ler.nextInt();
+        //int ano = 2020;
+        //System.out.println("A pessoa nascida em " + nasc + " tem " + (ano-nasc) + " anos.");
+
+        int i = 100;
+        boolean primo;
+        while(i < 200){
+            primo = true;
+            for(int j=2;j<i;j++){
+                if (i % j == 0){
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo){
+                System.out.println(i);
+                
+                }
+            
+            i++;
         }
     }
+
 }
